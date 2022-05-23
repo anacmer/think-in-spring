52a9b14480dbbbe7b627f9f6ef2d7896e931911c
zhaojq <1179492305@qq.com>
2022-05-23 23:20:58 +0800

    feat:three style for init bean

--- learn-spring-bean/src/main/java/com/anacmer/learn/spring/bean/factory/DefaultUserFactory.java
+++ learn-spring-bean/src/main/java/com/anacmer/learn/spring/bean/factory/DefaultUserFactory.java
@@ -1,8 +1,36 @@
 package com.anacmer.learn.spring.bean.factory;
 
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
 /**
  * @Author ZhaoJQ
  * @Date 2022/5/23 22:19
  */
-public class DefaultUserFactory implements UserFactory {
+public class DefaultUserFactory implements UserFactory, InitializingBean {
+
+    /**
+     * 三种方法初始化 Spring Bean
+     * 优先级分别如下
+     * 1.@PostConstruct
+     * 2.InitializingBean#afterPropertiesSet
+     * 3.BeanDefinition#initMethod
+     */
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("afterPropertiesSet(), UserFactory 初始化中...");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("@PostConstruct注解, UserFactory 初始化中...");
+    }
+
+
+    @Override
+    public void initUserFactory() {
+        System.out.println("自定义初始化方法 initUserFactory(), UserFactory初始化中...");
+    }
 }
--- learn-spring-bean/src/main/java/com/anacmer/learn/spring/bean/initial/BeanInitializationDemo.java
+++ learn-spring-bean/src/main/java/com/anacmer/learn/spring/bean/initial/BeanInitializationDemo.java
@@ -1,5 +1,41 @@
-package com.anacmer.learn.spring.bean.initial;/**
-  * @Author ZhaoJQ
-  * @Date 2022/5/23 23:02
- */public class BeanInitializationDemo {
+package com.anacmer.learn.spring.bean.initial;
+
+import com.anacmer.learn.spring.bean.factory.DefaultUserFactory;
+import com.anacmer.learn.spring.bean.factory.UserFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Bean Initialization Demo
+ *
+ * @Author ZhaoJQ
+ * @Date 2022/5/23 23:02
+ */
+@Configuration // 表示这是一个配置 bean
+public class BeanInitializationDemo {
+
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext()) {
+            applicationContext.register(BeanInitializationDemo.class);
+            // 启用 Spring 应用上下文
+            applicationContext.refresh();
+
+            UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+            System.out.println(userFactory.createUser());
+        }
+    }
+
+    /**
+     * 从 Spring 5.1 版本之后该方法 提炼到了 BeanDefinition 中
+     * 该方法映射了 @Bean 中 initMethod
+     * {@link org.springframework.beans.factory.support.AbstractBeanDefinition#setInitMethodName}
+     *
+     * @return UserFactory
+     */
+    @Bean(initMethod = "initUserFactory")
+    public UserFactory userFactory() {
+        return new DefaultUserFactory();
+    }
 }
